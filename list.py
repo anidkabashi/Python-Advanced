cd0233af8b1fa7c91ae2ed723ad838b4716f1f66
anidkabashi <anidkabashi@gmail.com>
2026-02-10 17:48:26 +0100

    .

---
+++ list.py
@@ -0,0 +1,15 @@
+to_do_list = ['make food', 'make my bed', 'go to school', 'do homework']
+first_item = to_do_list [0]
+second_item = to_do_list [1]
+third_item = to_do_list [2]
+fourth_item = to_do_list [3]
+
+print(first_item)
+print(second_item)
+print(third_item)
+print(fourth_item)
+
+shoping_list = ['apple', 'pineapple', 4, 5]
+shoping_list.append('orange')
+
+print(shoping_list)
---
+++ variable.py
@@ -0,0 +1,19 @@
+temperatur = 43.2
+name = 'Anid'
+
+print(temperatur)
+print(type(name))
+
+age = 16
+
+print("age is:", age)
+
+age=age+1
+
+print("age is:", age)
+
+first_name = 'Anid'
+last_name = 'Kabahshi'
+full_name = first_name + " " + last_name
+print(full_name)
+
