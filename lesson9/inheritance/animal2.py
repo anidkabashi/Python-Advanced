f12c5e06d202ccb45cff61c77e7ae49e16ebbfdd
anidkabashi <anidkabashi@gmail.com>
2026-03-17 17:35:06 +0100

    .

---
+++ lesson9/inheritance/animal2.py
@@ -0,0 +1,50 @@
+class Animal:
+    def __init__(self,name):
+        self.name=name
+
+    def sound(self):
+        print("Some generic animal sound")
+
+    def description(self):
+        print(f"This is an animal name {self.name}")
+
+class Dog(Animal):
+    def __init__(self,name,breed):
+        super().__init__(name)
+        self.breed = breed
+
+    def sound(self):
+        print("Woof!Woof!")
+
+    def description(self):
+        super().description()
+
+        print(f"Breed: {self.breed}")
+
+
+
+class Cat(Animal):
+    def __init__(self,name,color):
+        super().__init__(name)
+        self.color = color
+
+    def sound(self):
+        print("Meow!Meow!")
+
+    def description(self):
+        super().description()
+        print(f"Color: {self.color}")
+
+animal = Animal("Generic Animal")
+
+animal.sound()
+animal.description()
+
+dog = Dog("Bella", "golden retriever")
+dog.sound()
+dog.description()
+
+cat = Cat("Nimo", "White")
+cat.color()
+cat.description()
+
