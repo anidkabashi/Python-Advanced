f9b9bb583176cbceeee841b982a15e842810889a
anidkabashi <anidkabashi@gmail.com>
2026-03-26 17:40:39 +0100

    .

---
+++ lesson11/file_operations.py
@@ -0,0 +1,50 @@
+#file_path = "example.txt"
+# file = open(file_path, "r")
+#
+# content = file.read()
+# print(content)
+#
+# file.close()
+
+import os
+
+#Opeming and closing files
+file = open('example.txt', 'r')
+file.close()
+
+#using with statement for automatic closing
+with open('example.txt', 'r') as file:
+    content = file.read()
+
+#reading from files
+with open('example.txt', 'r') as file:
+    content = file.read() #read entire content
+    line = file.readline() #read a single line
+    lines = file.readlines() #read all lines into a list
+
+#writing to files
+with open('example.txt', 'w') as file:
+    file.write("Hello world!")
+
+lines = ['Hello world!\n', 'Welcome to Python\n']
+with open('example.txt', 'w') as file:
+    file.writelines(lines)
+
+#moving the cursor
+with open('example.txt', 'r') as file:
+    file.seek(0)
+    data = file.read()
+    print(data)
+
+#checking file existence
+if os.path.exists('example.txt'):
+    print('file exists')
+
+#appending to file
+with open('example.txt', 'a') as file:
+    file.write("New data appended")
+
+#reading and writing binary files
+data = b'this is some binary data'
+with open('example.bin', 'wb') as file:
+    file.write(data)
