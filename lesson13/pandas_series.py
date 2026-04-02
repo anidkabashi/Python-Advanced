dc733127fbefe351f7997087325c82566c6c901b
EnesNI <anidkabashi@gmail.com>
2026-04-02 17:37:30 +0200

    .

---
+++ lesson13/pandas_series.py
@@ -0,0 +1,21 @@
+from itertools import product
+
+import pandas as pd
+
+from lesson13.array_manipulacion import total_sum
+
+product = ['Apples', 'Bananas', 'Orange', 'Grapes', 'Pineapples']
+
+sales = [150,200,180,90,60]
+
+sales_series = pd.Series(sales, index=product)
+
+print(sales_series)
+
+print(sales_series['Grapes'])
+
+total_sales = sales_series.sum()
+print(total_sales)
+
+best_selling_product = sales_series.idxmax()
+print(f"Best selling product: {best_selling_product}")
