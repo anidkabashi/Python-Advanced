2a8e943943046965d2de4e7cc10cfcef3d5311f6
EnesNI <anidkabashi@gmail.com>
2026-04-28 17:41:32 +0200

    .

---
+++ lesson17/slidebar_layout.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.sidebar.header("Sidebar")
+
+st.sidebar.write("This is the sidebar")
+
+st.sidebar.selectbox("Chose an option", ["Option 1", "Opition 2", "Option 3"])
+
+st.sidebar.radio("Go to", ["Home", "Data", "Settings"])
+
