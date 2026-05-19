4265871fc923d2702deba7e9276d52e2d509b206
unknown <anidkabashi@gmail.com>
2026-05-19 17:41:41 +0200

    .

---
+++ lesson23/integrating_tools/main.py
@@ -0,0 +1,27 @@
+from random import sample
+
+from fastapi import FastAPI
+from fontTools.misc.cython import returns
+
+from models import Developer, Project
+
+app = FastAPI()
+
+@app.post("/developers/")
+def create_developer(developer: Developer):
+    return {"message": "Deceloper created siccessfully", "developer": developer}
+
+@app.post("/projects/")
+def create_project(project: Project):
+    return {"message": "Project created successfully", "project": project}
+
+@app.get("/projects/")
+def get_projects():
+
+    sample_project = Project(
+        title="Sample Project",
+        description= "This is a sample project"
+        languages=["Python", "Javascript"]
+        lead_developer=Developer(name="Dion Kabashi", experience=5)
+    )
+    return {"projects": [sample_project]}
